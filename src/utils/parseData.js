57788fdd804bcff0d900a03a7f9611bc852adffd
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-08-01 15:43:11 +0530

    lots of debugging..need a break

--- src/utils/parseData.js
+++ src/utils/parseData.js
@@ -1,19 +1,76 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import {parseVideoDuration } from './parseVideoDuration'
 
- const parseData = async (items) => {
-     console.log(items);
-     
-     const videoIDs = [];
-     const channelIDs = [];
+const API_KEY = process.env.REACT_APP_YOUTUBE_DATA_API_KEY;
 
-     items.forEach(element => {
-         channelIDs.push(element.snippet.channelID)
-         videoIDs.push(element.id.channelID);
-     });
-  return (
-      <div>
+const parseData = async (items) => {
+  try {
+    const videoIds = [];
+    const channelIds = [];
+
+    items.forEach((item) => {
+      channelIds.push(item.snippet.channelId);
+      videoIds.push(item.id.videoId);
+    });
+
+    const {
+      data: { items: channelsData },
+    } = await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/channels?part=snippet,contentDetails&id=${channelIds.join(
+            ","
+    )}&key=${API_KEY}`
+          );
       
-      </div>
-  )
-}
-export default parseData
+      console.log(channelsData)
+
+    const parsedChannelData = [];
+    channelsData.forEach((channel) =>
+      parsedChannelData.push({
+        id: channel.id,
+        image: channel.snippet.thumbnails.default.url,
+      })
+    );
+
+    const {
+      data: { items: videosData },
+    } =
+      await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id,statistics&id=${videoIds.join(
+            ","
+      )}&key=${API_KEY}`);
+      
+
+      console.log(videosData)
+    const parseData = [];
+    items.forEach((item, index) => {
+      const { image: channelImage } = parsedChannelData.find(
+        (data) => data.id === item.snippet.channelId
+      );
+      if (channelImage) {
+        parseData.push({
+          videoId: item.id.videoId,
+          videoTitle: item.snippet.title,
+          videoDescription: item.snippet.description,
+          videoThumbnail: item.snippet.thumbnails.medium.url,
+          videoLink: `https://www.youtube.com/watch=?v=${item.id.videoId}`,
+          videoDuration: parseVideoDuration(
+            videosData[index].contentDetails.duration
+          ),
+          videoViews: convertRawtoString(
+            videosData[index].statistics.viewCount
+          ),
+          videoAge: timeSince(new Date(item.snippet.publishedAt)),
+          channelInfo: {
+            id: item.snippet.channelId,
+            image: channelImage,
+            name: item.snippet.channelTitle,
+          },
+        });
+      }
+    });
+    return parseData;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export default parseData;
---
+++ src/utils/parseVideoDuration.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export const parseVideoDuration = (duration) => {
+    console.log(duration)
+  return (
+    <div>parseVideoDuration</div>
+  )
+}
+
