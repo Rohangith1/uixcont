6eab0c3d93884b6d70d68c9670d61b518d7cbab9
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-08-02 23:58:49 +0530

    error :(

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,45 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import {useAppDispatch,useAppSelector} from "../hooks/useApp"
+import { useAppDispatch, useAppSelector } from "../hooks/useApp";
 import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+import Spinner from "../components/Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Home = () => {
-
   const dispatch = useAppDispatch();
   const videos = useAppSelector((state) => state.ucountApp.videos);
   useEffect(() => {
-    dispatch(getHomePageVideos(false))
-    
+    dispatch(getHomePageVideos(false));
+
     console.log(videos);
-},[dispatch])
+  }, [dispatch]);
 
   return (
-    <div>
-      <Navbar />
-      <Sidebar />
+    <div className="max-h-screen overflow-auto">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={650}
+          ><div>
+              {
+                videos.map((item) => {
+                  return <Card data={item} key={item.videoId}/>
+                })
+            }
+            </div></InfiniteScroll>
+        ) : (
+          <Spinner />
+        )}
+      </div>
     </div>
   );
 };
