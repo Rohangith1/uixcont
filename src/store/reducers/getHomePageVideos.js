1cfd4f446940014dc5f7467072a4643bafde628a
Rohangith1 <107810397+Rohangith1@users.noreply.github.com>
2024-08-03 22:50:09 +0530

    fix nhi horaiii

--- src/store/reducers/getHomePageVideos.js
+++ src/store/reducers/getHomePageVideos.js
@@ -10,11 +10,11 @@ export const getHomePageVideos = createAsyncThunk(
     const {
       ucountApp: { nextPageToken: nextPageTokenFromState, videos },
     } = getState();
-    const response = await axios.get(
-      `https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q="drop x out"&key=${API_KEY}&part=snippet&type=video&${
-        isNext ? `pageToken=${nextPageTokenFromState}` : ""
-      }`
-    );
+     const response = await axios.get(
+       `https://youtube.googleapis.com/youtube/v3/search?maxResults=20&q="drop x out"&key=${API_KEY}&part=snippet&type=video&${
+         isNext ? `pageToken=${nextPageTokenFromState}` : ""
+       }`
+     );
     // console.log(response.data.items);
 
     const items = response.data.items;
@@ -25,6 +25,6 @@ export const getHomePageVideos = createAsyncThunk(
     return {
       parsedData: [...videos, ...parsedData],
       nextPageToken: nextPageTokenFromState,
-    };
+    }
   }
 );
